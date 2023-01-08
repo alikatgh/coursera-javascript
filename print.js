8db7d707d10bf5a02050f2d0a0825d539290eaea
alikatgh <i@alikn.ru>
2023-01-08 11:07:19 +0600

    looping back and forth

---
+++ print.js
@@ -0,0 +1,17 @@
+// simple counting
+
+function lets_count() {
+  for (var i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+
+// console.log("Go");
+
+function reverse_count() {
+  for (var i = 10; i > 0; i--) {
+    console.log(i);
+  }
+}
+lets_count();
+reverse_count();
