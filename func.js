b398f6d6de40fdc03d470c8e585285f6f6569db4
alikatgh <i@alikn.ru>
2023-01-14 22:00:27 +0600

    js files

---
+++ func.js
@@ -0,0 +1,11 @@
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+
+function al(arr) {
+  // comment
+  for (var i = 0; i < arr.length; i++) {
+    // code goes here
+    console.log(arr[i]);
+  }
+}
+
+al(colors); //display all items in the array at once
---
+++ teachers_page.js
@@ -0,0 +1,7 @@
+var cubes = "ASDLFKJDS";
+
+for (var i = 0; i < cubes.length; i++) {
+  var styles =
+    "font-size: 40px; border-radius: 10px; border: 1px solid blue; backround-color: pink; color: purple;";
+  console.log("%c" + cubes[i], styles);
+}
